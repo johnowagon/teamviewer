14c8d568e8d4ebb5b606523152663f348c2ca659
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-03-23 23:30:01 -0600

    not where index.js belongs...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App';
-import reportWebVitals from './src/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
