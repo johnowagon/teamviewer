16068f0a13c224903a98f9b2d4b01f7ad7b81e67
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-03-24 23:16:57 -0600

    this sucks

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,4 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors')
 
-app.use(cors({
-    origin: "https://johnowagon.github.io/teamviewer/",
-    credentials: true
-}))
+app.use(cors())
