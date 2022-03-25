268248f68d685e69eea918de5767217ccb341035
johnowagon <97626704+johnowagon@users.noreply.github.com>
2022-03-24 23:26:01 -0600

    more cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,4 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    "Access-Control-Allow-Origin": "https://johnowagon.github.io/teamviewer/",
+    "credentials": true
+}))
